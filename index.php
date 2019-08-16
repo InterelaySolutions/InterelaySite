5b095f4866a3a829fe52d8c199058e14146cc5b5
Unknown <support@interelay.com>
2019-08-16 02:41:03 +0100

    Site Upgrade v0.6
    -Updated navbar & logo, improved website display
    -Added material design

--- index.php
+++ index.php
@@ -1,8 +1,12 @@
 <html><head>
-        <link rel="stylesheet" type="text/css" href="/site/css/main.css">
         <title>Interelay Solutions - Computer Repairs, Managed IT Services, Network Administration and Anti-Virus Services in Kent</title>
-        <link href="https://fonts.googleapis.com/css?family=Libre+Franklin:400,700|Rubik:400,700" rel="stylesheet"> 
+        <link href="https://fonts.googleapis.com/css?family=Roboto:400,700|Rubik:400,700" rel="stylesheet">
+        <link href="https://fonts.googleapis.com/css?family=Montserrat&display=swap" rel="stylesheet">  
         <script src="js/jquery.js"></script>
+        <link href="https://unpkg.com/material-components-web@latest/dist/material-components-web.min.css" rel="stylesheet">
+        <script src="https://unpkg.com/material-components-web@latest/dist/material-components-web.min.js"></script>
+        <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
+        <link rel="stylesheet" type="text/css" href="css/main.css">
         <script>
             // handle links with @href started with '#' only
             $(document).on('click', 'a[href^="#"]', function(e) {
@@ -20,6 +24,10 @@
     
                 //  top position relative to the document
                 var pos = $id.offset().top;
+
+                if (id == "#top") {
+                    pos = pos -120;
+                }
     
                 // animated top scrolling
                 $('body, html').animate({scrollTop: pos});
@@ -27,44 +35,45 @@
   </script>
   <style>
   h1 {
-      font-family: 'Libre Franklin';
-      font-size: 52px;
-      padding-top: 60px;
-
+      font-family: 'Roboto';
+      font-size: 32px;
   }
   
   </style>
     </head>
     <body style="background-color:black;">
-        <div id="navbar" class="navbar"><a href="#top">Return to Top</a> | <a href="#aboutus">About Us</a> | <a href="#contactus">Contact Us</a>  <div id="loginpane" style="float:right; margin-top:-7;"> <!--<a href="login.php">Login and book an appointment</a> | ---><a href="/osTicket/">Support Helpdesk</a> 
+    <div class="logolinkbox" style="position:fixed; left:0px; top:0px; width:310px; height:77px;float: left;z-index:1"><a style="position:fixed; left:0px; top:0px; width:310px; height:77px;float: left;z-index:1" href="./" >&nbsp;</a></div>
+    <div id="logobar" class="logobar"><img src="images/logo.png" style="float:left; height:60px; margin-right:7px; margin-left:7px;"/><h1 style="color:white; float:left; margin:14px; font-family: 'Montserrat'">Interelay</h1> <p style="color:white; position:relative; margin:14px; font-family: 'Montserrat'; top: 25px;left: -11px;">Solutions</p></div>
+        <div id="navbar" class="navbar">
+            <button class="mdc-button mdc-button--raised" style="margin-left:10px;"><a href="#top">Return to Top</a></button> <button class="mdc-button mdc-button--raised"><a href="#aboutus">About Us</a></button> <button class="mdc-button mdc-button--raised"><a href="#contactus">Contact Us</a></button>   <div id="loginpane" style="float:right; margin-top:-7;"> <!--<a href="login.php">Login and book an appointment</a> | ---><button class="mdc-button mdc-button--raised" style="margin-right:10px;"><a href="/osTicket/">Support Helpdesk</a></button> 
 			
 			
 			</div></div>
         <!-- Slide #1 -- Three Divs aligned into three columns -->
-        <div class="slide" style="background-image: url('/site/images/circuit-slide1-v1.jpg'); background-repeat:no-repeat; background-size:cover; height:1200px; position:relative; top:-34px; padding-bottom:0px;">
+        <div class="slide" style="background-image: url('images/serverroom-slide1-v2.jpg'); background-repeat:no-repeat; background-size:cover; position:relative; top:-50px; padding-bottom:50px;">
             <a class="smoothScroll" id="top"></a>
-            <h1>Interelay Solutions</h1>
 <br><br>
             <div id="three_way_container">
-                <div class="equal-div33"><img src="/site/images/antivirus.png" style="width:100%; max-width:150px; height:auto;"><h3>Key Results with our Virus Removal Service</h3><p>Interelay Solutions uses effective commerical grade Anti-Virus software to remove damaging infections on both home PCs and networked, domain-level business infrastructure.  As part of our service, we will ensure that you are completely protected from viruses, trojans and other invasive vectors to your system.  We can gaurentee that we will take every effort to save and restore damaged data from the attack and offer backup solutions to prevent damage in the future.</p></div><div class="equal-div33"><img src="/site/images/Circle-icons-computer.png" style="width:100%;max-width:150px; height:auto;"><h3>Hardware and Software Challenges</h3><p>Our IT support technicians has over ten years of experience in handling software and hardware faults.  Performing ad-hoc repairs and ensure a resolution before leaving the premises.  We provide a call-out service for home users and a contracted service for regular maintenence of business infrastructure (Hardware and Software based).  Contact us now too see what managed IT solutions we can provide for you, or if you're a customer, please call now and book an appointment!</p></div><div class="equal-div33"><img src="/site/images/network.png" style="width:100%;max-width:150px; height:auto;"><h3>Providing Networking and Administrative services</h3><p>Our technicians have considerable experience in providing Network Administrative assistance, ranging from Active Directory to Windows Server.  We can provide IT management solutions for your business, removing the need to manage you network infrastructure within the company, saving you time, money and resources.</p></div>
+                <div class="equal-div33"><img src="images/softdev.png" style="width:100%;max-width:150px; height:auto;"><h3>Bespoke software development</h3><p>We have a proven track record to develop custom software solutions for your company.  We perform a design brief for you which outlines the features of your solution and how to integrate it into your regular workflows.  We can create a full-stack custom solution for you, using proven technologies (PHP, Python, CSS, SQL, Javascript/JQuery)</p></div>
+                <div class="equal-div33"><img src="images/website-design.png" style="width:100%; max-width:150px; height:auto;"><h3>IT Services Consultancy</h3><p>Not sure how you want to get from A to B?  We can help provide consulations which will provide an informed pathway to get you to your goals.  IT project management can be tricky and overly costly, let us put you on the right track to avoid redevelopment or recreation at a later stage.</p></div><div class="equal-div33"><img src="images/Circle-icons-computer.png" style="width:100%;max-width:150px; height:auto;"><h3>Hardware and Software Challenges</h3><p>Our IT support technicians has over ten years of experience in handling software and hardware faults.  Performing ad-hoc repairs and ensure a resolution before leaving the premises.  We provide a call-out service for home users and a contracted service for regular maintenence of business infrastructure (Hardware and Software based).  Contact us now too see what managed IT solutions we can provide for you, or if you're a customer, please call now and book an appointment!</p></div><div class="equal-div33"><img src="images/network.png" style="width:100%;max-width:150px; height:auto;"><h3>Providing Networking and Administrative services</h3><p>Our technicians have considerable experience in providing Network Administrative assistance, ranging from Active Directory to Windows Server to Linux.  We can provide IT management solutions for your business, removing the need to manage your network infrastructure within the company, saving you time, money and resources.</p></div>
             </div>
         </div>
         <!-- Slide #2 -- Side by Side Divs arranged in a stacked format -->
-        <div class="slide_inverted" style="background-image: url('/site/images/slide2_v2.jpg'); background-repeat:no-repeat; background-size:cover;top:-29px;position: relative;">
+        <div class="slide_inverted" style="background-image: url('images/slide2_v2.jpg'); background-repeat:no-repeat; background-size:cover;top:-29px;position: relative; padding-top:20px; padding-bottom:40px">
             <div class="container">
                 <h2>We also offer...</h2>
-                <div class="slide2_right"><h2>Data Recovery</h2><p>Our extensive experience with data recovery tools gives our technicians a unique insight how to effectively recover data.  With cloud storage becoming increasingly popular, we provide consultancy services to ensure you find the right cloud service to meet your companies needs.</p></div> <div class="slide2_left" style=""><img src="/site/images/data_recovery.png" style="width:100%; max-width:150px; height:auto;"></div>
-                <div class="slide2_right"><h2>Customised PCs, built by us, for you</h2><p>We provide a hardware-requirements consultancy service, where we will assess your needs and build a computer specifically tailored to your needs.  Our PCs come with a one-year warranty and free telephone customer support, helping you every step of the way, making sure you're happy with the hardware.</p></div><div class="slide2_left"><img src="/site/images/custompc.png" style="width:100%; max-width:150px; height:auto;"></div>
-                <div class="slide2_right"><h2>Bespoke Website Design</h2><p>Need a website that has specific requirements? Need a custom performance driven site that is in-line with your companies aims? Then look no further!  We provide a complete website design and creation experience, we will integrate any platform or app into your website and provide a website tailored to your specific needs.  We will provide a design brief before any work is carried out and work with you to create the exact website you need.</p></div><div class="slide2_left"><img src="/site/images/Circle-icons-browser.png" style="width:100%; max-width:150px; height:auto;"></div>
+                <div class="slide2_right"><h2>Data Recovery</h2><p>Our extensive experience with data recovery tools gives our technicians a unique insight how to effectively recover data.  With cloud storage becoming increasingly popular, we provide consultancy services to ensure you find the right cloud service to meet your companies needs.</p></div> <div class="slide2_left" style=""><img src="images/data_recovery.png" style="width:100%; max-width:150px; height:auto;"></div>
+                <div class="slide2_right"><h2>Customised PCs, built by us, for you</h2><p>We provide a hardware-requirements consultancy service, where we will assess your needs and build a computer specifically tailored to your needs.  Our PCs come with a one-year warranty and free telephone customer support, helping you every step of the way, making sure you're happy with the hardware.</p></div><div class="slide2_left"><img src="images/custompc.png" style="width:100%; max-width:150px; height:auto;"></div>
+                <div class="slide2_right"><h2>Complete website app solution</h2><p>Need a website that has specific requirements? Need a custom performance driven site that is in-line with your companies aims? Then look no further!  We provide a complete website design and creation experience, we will integrate any platform or app into your website and provide a website tailored to your specific needs.  We will provide a design brief before any work is carried out and work with you to create the exact website you need.</p></div><div class="slide2_left"><img src="images/Circle-icons-browser.png" style="width:100%; max-width:150px; height:auto;"></div>
             </div>
             
         </div>
         <!-- Dummy Div to create a precise anchor for the About Us Link -->
         <div id="aboutus" style="margin:0px; padding:0px; font-size:0px; position:relative; top:-20px;"></div>
         <!-- Slide #3 -- Information Dump Slide -->
-        <div class="slide" style="background-image: url('/site/images/slide3_v1.jpg'); background-repeat:no-repeat; background-size:cover; margin:0px; position: relative;top: -47px;padding-top: 10px;margin-top: 20px">
+        <div class="slide" style="background-image: url('images/slide3_v1.jpg'); background-repeat:no-repeat; background-size:cover; margin:0px; position: relative;top: -47px;padding-top: 10px;margin-top: 20px; padding-bottom:100px;">
             <div class="container">
-                <h2 style="text-align:left;">We have been providing services to community for over 10 years</h2>
+                <h2 style="text-align:left;color:white">We have been providing services to community for over 10 years</h2>
                 <div class="slide3"><h2>Our Reputation</h2>For the past ten years, we have been providing cost-effective solutions to the local community, providing call out services and fixing IT problems.  Ranging from virus removal to computer maintence, we provide an adapatable and flexible service to you and pride ourselves on doing our job well.  </div>
                 <div class="slide3"><h2>Call out service</h2>One of our most unique features is our call out service, we will come to you, at a time of your choosing.  We will come to you after work hours if required to ensure no disruption to your daily life.  Call us now to arrange an appointment to come to you and in 90% of all cases, we will provide a fix on-site.  Even if we don't, we will take your computer away, fix it on our premises and deliver it back to you</div>
                 <div class="slide3"><h2>No Fix, No Fee!</h2>If we cannot provide a fix to your computer,we will not charge you for our services, this means there is no risk to you and proves our ability to provide a solution to any situation or problem.  We are proud to say that in the vast majority of cases, we do provide an effective solution, but even if we can't there is no risk to you.</div>
@@ -74,9 +83,8 @@
         <div id="footer" style="position:relative; top:-46px;">
             <a class="smoothScroll" id="contactus"></a>
             <div class="container" style="color:grey; text-align:center;">
-                <h2 style="text-align:left;">Contact us now for an appointment</h2>
+                <h2 style="text-align:left;">Contact us now for an consultation</h2>
                 <div id="footer_left"><h4>Address:</h4>
-                Flat 6<br>
                 4 Hamond Hill<br>
                 Chatham<br>
                 Kent<br>
@@ -85,10 +93,9 @@
                 <h4>Call Us:</h4>
                 07999 047488<br>
                 01634 815500<br>
-                <a href="skype://interelay">SkypeCall</a><br>
                 <br>
                 <h4>Email Us:</h4>
-                <a href="mailto:support@interelay.com">support@interelay.com</a>
+                <a href="mailto:support@interelay.com" style="color: grey; text-decoration:underline">support@interelay.com</a>
                 </div>
                 </div>
             <div style="clear:both; text-align:center; font-size:x-small;">All Code and Images are Copyright Interelay Solutions - 2018</div>
