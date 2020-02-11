da55f8ed213cf2ea7f57d162576960effcd6b51d
David Ward <support@interelay.com>
2020-02-11 22:49:56 +0000

    cred

--- contact-us.php
+++ contact-us.php
@@ -109,7 +109,7 @@
 use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\SMTP;
 require_once "vendor/autoload.php";
-include("../credentials/creds.php");
+include( __DIR__.'/../credentials/creds.php');
 
 //PHPMailer Object
 $mail = new PHPMailer;
