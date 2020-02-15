7015abb780bd017ce145ef29abbfde087ed5d7c8
David Ward <support@interelay.com>
2020-02-15 03:36:49 +0000

    Header + Improved dropdown menu
    -header is now an include
    -added hover effects to the menu bar

---
+++ includes/header.php
@@ -0,0 +1,32 @@
+<div class="logolinkbox" style="position:fixed; left:0px; top:0px; width:310px; height:77px;float: left;z-index:1"><a style="position:fixed; left:0px; top:0px; width:310px; height:77px;float: left;z-index:1" href="./" >&nbsp;</a></div>
+    <div id="logobar" class="logobar"><img src="images/logo.png" style="float:left; height:60px; margin-right:7px; margin-left:7px;"/><h1 style="color:white; float:left; margin:14px; font-family: 'Montserrat'">Interelay</h1> <p style="color:white; position:relative; margin:14px; font-family: 'Montserrat'; top: 25px;left: -11px;">Solutions</p></div>
+        <div id="navbar" class="navbar">
+            <nav>
+                <div style="left: -40px;position: relative;">
+                <ul>
+                    <li><a href="#top" class='button-hover' style='padding: 15px;top: -18px;position: relative;'>Return to Top</a> 
+                    </li>
+                    <li><a class='button-hover' href="#aboutus" style='padding: 15px;top: -18px;position: relative;'>About Us</a>  
+                    </li>
+                    <li><a class='button-hover' href="#contactus" style='padding: 15px;top: -18px;position: relative;'>Contact Us</a> 
+                    </li>
+                    <li><a class='button-hover' href="#" style='padding: 15px;top: -18px;position: relative;'>Software Development</a>
+                    <ul class="dropdown">
+                        <li><a class='button-hover' href="academic-management-system" style='padding: 15px;top:-20px;left:338px;position: relative;'>Academic Management System</a></li>
+                        <!--<li><a class='button-hover' href="salsa-website" style='padding: 15px;top:-20px;left:332px;position: relative;'>Salsa Website</a></li>-->
+                        <li><a class='button-hover' href="quality-reporting-site" style='padding: 15px;top:-20px;left:338px;position: relative;'>Quality Reporting Site</a></li>
+                    </ul> 
+                    <li><a class='button-hover' href="#contactus" style='padding: 15px;top: -18px;position: relative;'>IT Services</a> 
+                    <ul class="dropdown">
+                      <li><a class='button-hover' href="computer-repair" style='padding: 15px;top:-20px;left:530px;position: relative;'>Computer Repair</a></li>
+                        <li><a class='button-hover' href="managed-it-services" style='padding: 15px;top:-20px;left:530px;position: relative;'>Managed IT Services</a></li>
+                        <li><a class='button-hover' href="network-vpn-management" style='padding: 15px;top:-20px;left:530px;position: relative;'>Network & VPN Management</a></li>
+                        <li><a class='button-hover' href="it-consultancy" style='padding: 15px;top:-20px;left:530px;position: relative;'>Consultancy</a></li>
+                    </ul>
+                    </li>
+                    </li>
+                </ul> 
+        </div> <div id="loginpane" style="float:right; margin-top:-7;"> <!--<a href="login.php">Login and book an appointment</a> | ---><a class='button-hover' style='padding: 14px;top: -22px;position: relative;' href="/osTicket/">Support Helpdesk</a> 
+            <nav>    
+        </div>
+    </div>
